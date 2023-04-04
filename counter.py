c1a3d55de9165223d7e2595a37eea11f2ccebea1
Samantha Tartamella <Samantha.Tartamella@vu.com>
2023-04-04 14:16:52 -0500

    counter

--- counter.py
+++ counter.py
@@ -2,6 +2,35 @@ def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
 
+    try : 
+        fhand = open(file_name)
+    except: 
+        print("Please enter valid file name.")
+        quit
+    
+    days = dict()
+
+    for line in fhand :
+        line = line.rstrip()
+        if line.startswith("From:") :
+            continue
+        if line.startswith("From") :
+            line =line.split()
+            if line[2] not in days :
+                days[line[2]] = 1
+            else : 
+                days[line[2]] = days[line[2]] + 1 
+
+    print(days)      
+            
+        
+
+
+
+
+
+
+
 
 
 ## if you want to test locally run > python payCalculator.py
